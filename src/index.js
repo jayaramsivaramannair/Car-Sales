1a50bfbc6cdf2f1aa4fc70b81d45bcda533f00da
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-02-28 07:01:41 -0500

    Set up the store and provider, moved initial state to a separate reducer file and connected components

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import featureReducer from './Reducers/featureReducer';
+
+const store = createStore(featureReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}> <App /></Provider>, rootElement);
